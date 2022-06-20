600048ff792c9ab754c4e7dde38299e3377e9f33
AcTiV-Project <98194613+AcTiV-Project@users.noreply.github.com>
2022-06-19 21:27:10 -0500

    Add files via upload

---
+++ SITIO WEB/Main.js
@@ -0,0 +1,76 @@
+$("document").ready(function(){
+    //alert("jQuery Cargado...");
+
+    //btn
+    $(".btn").mouseover(function(){
+        $(this).css("color","black");
+    });
+    $(".btn").mouseout(function(){
+        $(this).css("color","white");
+    });
+
+    //list1
+    $("#list1").mouseover(function(){
+        $(this).css("color","black");
+    });
+    $("#list1").mouseout(function(){
+        $(this).css("color","white");
+    });
+
+    //list2
+    $("#list2").mouseover(function(){
+        $(this).css("color","black");
+    });
+    $("#list2").mouseout(function(){
+        $(this).css("color","white");
+    });
+
+    //list3
+    $("#list3").mouseover(function(){
+        $(this).css("color","black");
+    });
+    $("#list3").mouseout(function(){
+        $(this).css("color","white");
+    });
+
+    //list4
+    $("#list4").mouseover(function(){
+        $(this).css("color","black");
+    });
+    $("#list4").mouseout(function(){
+        $(this).css("color","white");
+    });
+
+    //list5
+    $("#list5").mouseover(function(){
+        $(this).css("color","black");
+    });
+    $("#list5").mouseout(function(){
+        $(this).css("color","white");
+    });
+
+    //list6
+    $("#list6").mouseover(function(){
+        $(this).css("color","black");
+    });
+    $("#list6").mouseout(function(){
+        $(this).css("color","white");
+    });
+
+    //list7
+    $("#list7").mouseover(function(){
+        $(this).css("color","black");
+    });
+    $("#list7").mouseout(function(){
+        $(this).css("color","white");
+    });
+
+    //list8
+    $("#list8").mouseover(function(){
+        $(this).css("color","black");
+    });
+    $("#list8").mouseout(function(){
+        $(this).css("color","white");
+    });
+
+});
